30fb48f4a2b68913558f1ca14f4145e711b563db
OPvitzz <163419898+OPvitzz@users.noreply.github.com>
2024-11-07 09:58:45 -0300

    Add files via upload

---
+++ java/teste.js
@@ -0,0 +1,5 @@
+let numero1 = parseInt(prompt("Digite o primeiro numero: "));
+let numero2 = parseInt(prompt("Digite o segundo numero: "))     ;
+let soma = numero1 + numero2;
+document.write("A soma é igual a: ", soma);
+console.log("O valor da soma no conole é igual a: ", soma);          
